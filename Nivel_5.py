e111785e579c094b8ab81ee70c9f4d8344d0af3a
Bryan <brquinterocu@gmail.com>
2025-11-07 13:38:23 -0500

    updated 2

--- Nivel_5.py
+++ Nivel_5.py
@@ -1,92 +1,92 @@
-# #25. Sistema de calificaciones.
+#25. Sistema de calificaciones.
 
-# print("====== Sistema de calificaciones ======")
-# lista_estudiantes = []
-# opcion = 0
-# calificaciones = {
-#     "Nombre Alumno" : "",
-#     "Calificaciones" : []
-# }
+print("====== Sistema de calificaciones ======")
+lista_estudiantes = []
+opcion = 0
+calificaciones = {
+    "Nombre Alumno" : "",
+    "Calificaciones" : []
+}
 
-# while True:
-#     print("1. Registrar usuario y notas")
-#     print("2. Consultar notas por usuario")
-#     print("3. Salir ")
+while True:
+    print("1. Registrar usuario y notas")
+    print("2. Consultar notas por usuario")
+    print("3. Salir ")
     
-#     opcion = int(input("Ingrese la opcion deseada: "))
+    opcion = int(input("Ingrese la opcion deseada: "))
 
-#     if opcion == 1:
-#         name = input("Ingrese el nombre del alumno: ")
-#         notas = []
-#         for i in range(5):
-#             nota = float(input(f"Ingrese la {i + 1} nota del alumno: "))
-#             notas.append(nota)
-#         calificaciones = {"Nombre Alumno" : name, "Calificaciones" : notas}
-#         lista_estudiantes.append(calificaciones)
-#     elif opcion == 2:
-#         name1 = input("Ingrese el nombre del alumno que quiere ver la notas: ")
-#         encontrado = False
+    if opcion == 1:
+        name = input("Ingrese el nombre del alumno: ")
+        notas = []
+        for i in range(5):
+            nota = float(input(f"Ingrese la {i + 1} nota del alumno: "))
+            notas.append(nota)
+        calificaciones = {"Nombre Alumno" : name, "Calificaciones" : notas}
+        lista_estudiantes.append(calificaciones)
+    elif opcion == 2:
+        name1 = input("Ingrese el nombre del alumno que quiere ver la notas: ")
+        encontrado = False
 
-#         for estudiante in lista_estudiantes:
-#             if estudiante["Nombre Alumno"].lower() == name1.lower():
-#                 print(f"Las notas del estudiante {estudiante['Nombre Alumno']} son: {estudiante["Calificaciones"]}")
-#                 encontrado = True
-#                 break
-#         if not encontrado:
-#             print(f"El alumno {name1} no se encuentra en la lista.\n")
-#     elif opcion == 3:
-#         break
-#     else:
-#         print("Opcion invalida, intente de nuevo.")
+        for estudiante in lista_estudiantes:
+            if estudiante["Nombre Alumno"].lower() == name1.lower():
+                print(f"Las notas del estudiante {estudiante['Nombre Alumno']} son: {estudiante["Calificaciones"]}")
+                encontrado = True
+                break
+        if not encontrado:
+            print(f"El alumno {name1} no se encuentra en la lista.\n")
+    elif opcion == 3:
+        break
+    else:
+        print("Opcion invalida, intente de nuevo.")
 
-# #26. Carrito de compras.
+#26. Carrito de compras.
 
-# print("====== Carrito de Compras ======")
-# carrito_compras = []
-# opcion = 0
-# contador = 0.0
-# articulo = {
-#     "Nombre Producto" : "",
-#     "Precio producto" : 0.0
-# }
+print("====== Carrito de Compras ======")
+carrito_compras = []
+opcion = 0
+contador = 0.0
+articulo = {
+    "Nombre Producto" : "",
+    "Precio producto" : 0.0
+}
 
-# while True:
-#     print("1. Añdir articulo")
-#     print("2. Consultar articulos en el carrito")
-#     print("3. Eliminar articulo")
-#     print("4. Valor total de la compra")
-#     print("5. Salir ")
+while True:
+    print("1. Añdir articulo")
+    print("2. Consultar articulos en el carrito")
+    print("3. Eliminar articulo")
+    print("4. Valor total de la compra")
+    print("5. Salir ")
     
-#     opcion = int(input("Ingrese la opcion deseada: "))
+    opcion = int(input("Ingrese la opcion deseada: "))
 
-#     if opcion == 1:
-#         name = input("Ingrese el nombre del producto: ")
-#         price = float(input("Ingrese el precio del producto: "))
-#         articulo = {"Nombre Producto" : name, "Precio producto" : price}
-#         carrito_compras.append(articulo)
-#     elif opcion == 2:
-#         print("Actualmente en el carrito estan los siguientes articulos: \n")
-#         print(carrito_compras)
-#     elif opcion == 3:
-#         name1 = input("Ingrese el nombre del articulo que desea eliminar: ")
-#         encontrado = False
-#         for compra in carrito_compras:
-#             if compra["Nombre Producto"].lower() == name1.lower():
-#                 carrito_compras.remove(compra)
-#                 print(f"El articulo {name1} ha sido removido del carrito.")
-#                 encontrado = True
-#                 break
-#         if not encontrado:
-#             print(f"El articulo {name1} no se pudo eliminar ya que no se encuentra en el carrito")
+    if opcion == 1:
+        name = input("Ingrese el nombre del producto: ")
+        price = float(input("Ingrese el precio del producto: "))
+        articulo = {"Nombre Producto" : name, "Precio producto" : price}
+        carrito_compras.append(articulo)
+    elif opcion == 2:
+        print("Actualmente en el carrito estan los siguientes articulos: \n")
+        print(carrito_compras)
+    elif opcion == 3:
+        name1 = input("Ingrese el nombre del articulo que desea eliminar: ")
+        encontrado = False
+        for compra in carrito_compras:
+            if compra["Nombre Producto"].lower() == name1.lower():
+                carrito_compras.remove(compra)
+                print(f"El articulo {name1} ha sido removido del carrito.")
+                encontrado = True
+                break
+        if not encontrado:
+            print(f"El articulo {name1} no se pudo eliminar ya que no se encuentra en el carrito")
 
-#     elif opcion == 4:
-#         for compra in carrito_compras:
-#             contador += compra["Precio producto"]
-#         print(f"En el carrito hay {len(carrito_compras)} articulos con valor total de {contador} pesos")
-#     elif opcion == 5:
-#         break
-#     else:
-#         print("Opcion invalida, intente de nuevo.")
+    elif opcion == 4:
+        for compra in carrito_compras:
+            contador += compra["Precio producto"]
+        print(f"En el carrito hay {len(carrito_compras)} articulos con valor total de {contador} pesos")
+    elif opcion == 5:
+        break
+    else:
+        print("Opcion invalida, intente de nuevo.")
 
 #27. Cajero automático.
 
@@ -125,6 +125,23 @@ while True:
 
 #28. Gestión de estudiantes (mini base de datos).
 
+print("Bienvenido al sistema de gestion de estudiantes de RIWI")
+
+tabla_usuarios = {0 : {"name": "", "email": "", "clan" : ""}}
+opcion = 0
+while True:
+    print("1. Agregar un nuevo registro")
+    name = input("Ingrese el nombre del alumno: ")
+    email = input("Ingrese el email del alumno: ")
+    clan = input("Ingrese el clan al que pertenece el alumno: ")
+
+    print("2. Eliminar un registro")
+    print("3. Mostrar Todos los Registros")
+    print("4. Buscar estudiante por ID")
+    print("5. Modificar informacion de estudiante")
+    print("6. Salir")
 #29. Calculadora avanzada (usar funciones).
 
 #30. Agenda de contactos (lista de diccionarios).
+
+# https://github.com/daveshb/riwipython/blob/main/modulo1/semana1/Taller1.md
