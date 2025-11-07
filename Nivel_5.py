93c419b0fc435bf5c34c20a7cda7df137ce1c855
Bryan <brquinterocu@gmail.com>
2025-11-07 13:34:52 -0500

    Actualizacion taller basico

--- Nivel_5.py
+++ Nivel_5.py
@@ -39,56 +39,89 @@
 #     else:
 #         print("Opcion invalida, intente de nuevo.")
 
-#26. Carrito de compras.
+# #26. Carrito de compras.
 
-print("====== Carrito de Compras ======")
-carrito_compras = []
-opcion = 0
-contador = 0.0
-articulo = {
-    "Nombre Producto" : "",
-    "Precio producto" : 0.0
-}
+# print("====== Carrito de Compras ======")
+# carrito_compras = []
+# opcion = 0
+# contador = 0.0
+# articulo = {
+#     "Nombre Producto" : "",
+#     "Precio producto" : 0.0
+# }
 
-while True:
-    print("1. Añdir articulo")
-    print("2. Consultar articulos en el carrito")
-    print("3. Eliminar articulo")
-    print("4. Valor total de la compra")
-    print("5. Salir ")
+# while True:
+#     print("1. Añdir articulo")
+#     print("2. Consultar articulos en el carrito")
+#     print("3. Eliminar articulo")
+#     print("4. Valor total de la compra")
+#     print("5. Salir ")
     
-    opcion = int(input("Ingrese la opcion deseada: "))
+#     opcion = int(input("Ingrese la opcion deseada: "))
 
-    if opcion == 1:
-        name = input("Ingrese el nombre del producto: ")
-        price = float(input("Ingrese el precio del producto: "))
-        articulo = {"Nombre Producto" : name, "Precio producto" : price}
-        carrito_compras.append(articulo)
-    elif opcion == 2:
-        print("Actualmente en el carrito estan los siguientes articulos: \n")
-        print(carrito_compras)
-    elif opcion == 3:
-        name1 = input("Ingrese el nombre del articulo que desea eliminar: ")
-        encontrado = False
-        for compra in carrito_compras:
-            if compra["Nombre Producto"].lower() == name1.lower():
-                carrito_compras.remove(compra)
-                print(f"El articulo {name1} ha sido removido del carrito.")
-                encontrado = True
-                break
-        if not encontrado:
-            print(f"El articulo {name1} no se pudo eliminar ya que no se encuentra en el carrito")
+#     if opcion == 1:
+#         name = input("Ingrese el nombre del producto: ")
+#         price = float(input("Ingrese el precio del producto: "))
+#         articulo = {"Nombre Producto" : name, "Precio producto" : price}
+#         carrito_compras.append(articulo)
+#     elif opcion == 2:
+#         print("Actualmente en el carrito estan los siguientes articulos: \n")
+#         print(carrito_compras)
+#     elif opcion == 3:
+#         name1 = input("Ingrese el nombre del articulo que desea eliminar: ")
+#         encontrado = False
+#         for compra in carrito_compras:
+#             if compra["Nombre Producto"].lower() == name1.lower():
+#                 carrito_compras.remove(compra)
+#                 print(f"El articulo {name1} ha sido removido del carrito.")
+#                 encontrado = True
+#                 break
+#         if not encontrado:
+#             print(f"El articulo {name1} no se pudo eliminar ya que no se encuentra en el carrito")
 
-    elif opcion == 4:
-        for compra in carrito_compras:
-            contador += compra["Precio producto"]
-        print(f"En el carrito hay {len(carrito_compras)} articulos con valor total de {contador} pesos")
-    elif opcion == 5:
-        break
-    else:
-        print("Opcion invalida, intente de nuevo.")
+#     elif opcion == 4:
+#         for compra in carrito_compras:
+#             contador += compra["Precio producto"]
+#         print(f"En el carrito hay {len(carrito_compras)} articulos con valor total de {contador} pesos")
+#     elif opcion == 5:
+#         break
+#     else:
+#         print("Opcion invalida, intente de nuevo.")
 
 #27. Cajero automático.
+
+print("Bienvenido al cajero de RIWI")
+saldo = 1000000.0
+opcion = 0
+while True:
+    print("1. Desea hacer un retiro de su cuenta")
+    print("2. Desea hacer un deposito a su cuenta")
+    print("3. Desea consultar su saldo actual")
+    print("4. Salir del cajero")
+
+    opcion = int(input("Ingrese el numero de la accion que desea realizar: "))
+
+    if opcion == 1:
+        monto = float(input("Ingrese el valor que desea retirar: "))
+        if monto <= saldo:
+            saldo = saldo - monto
+            print(f"Se han retirado {monto} pesos de su cuenta.\n")
+            print(f"El saldo restante en su cuenta es {saldo} $.")
+        else:
+            print(f"En su cuenta no hay dinero suficiente para retirar {monto} $.\n")
+    elif opcion == 2:
+        monto = float(input("Ingrese el valor que desea depositar: "))
+        saldo = saldo + monto
+        print(f"Su deposito fue exitoso, se han agregado {monto} $.\n")
+        print(f"Su nuevo saldo es {saldo} $.")
+    elif opcion == 3:
+        print(f"Su saldo actual es {saldo} $.")
+    elif opcion == 4:
+        print("Gracias por usar el cajero RIWI")
+        break
+    else:
+        print("opcion invalida, vuelva a intentarlo.")
+
 
 #28. Gestión de estudiantes (mini base de datos).
 
